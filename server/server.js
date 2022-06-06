3e90e17e4604f417d1111e904d39d23afecf068c
Nero Tiến <kur0dk0@gmail.com>
2022-06-06 20:00:07 +0700

    sth-news

--- server/server.js
+++ server/server.js
@@ -1,35 +1,40 @@
 const express = require ('express')
-const app = express()
+const cors = require ('cors')
+
+
 const http = require('http')
 const {Server} = require('socket.io')
-const server = http.createServer(app)
 const mongoose = require ('mongoose')
-const cors = require ('cors')
+
+const bodyparser =require ('body-parser')
+const server = http.createServer(app)
 const helmet = require('helmet')
 const dotenv = require('dotenv')
+const app = express()
+
 dotenv.config()
 
-const port = process.env.port ||3333
 const io = new Server(server,{
     cors:{
         origin: "http://localhost:3333",
         methods:["GET","POST"]
     }
 }) 
+
 app.use(helmet())
 app.use(cors())
 app.use(express.json())
-
-
 
 app.get("/",(req,res)=>{
     res.send("Hello")
 })
 
-app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" });
-  });
+//API
+const user = require('/api/users');
+app.use('/api/users', users)
 
+
+/////////////////////
 io.on('connection', (socket) => {
     console.log('User connected',socket.id);
     socket.on('disconnect', () => {
@@ -37,8 +42,8 @@ io.on('connection', (socket) => {
     });
   });
 
-
-
+////////////////////////////////////
+const port = process.env.port ||3333
 server.listen(port, ()=> {
     console.log('Server is listen to port:', port)
 })
