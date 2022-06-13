0f78e9fee1104c7b771fbf6b400851c8a7b16bae
Nero Tiến <kur0dk0@gmail.com>
2022-06-14 00:04:14 +0700

    change some file

--- client/src/index.js
+++ client/src/index.js
@@ -1,25 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {BrowserRouter,Routes, Route} from 'react-router-dom';
 import App from './App';
-import Header from './components/header/Header';
-import Login from './pages/login/login';
-import Register from './pages/register/Register'; 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter>
-        <Header/>
-        <Routes>
-            <Route path='/' element = {<App/>}/>
-            <Route path='/login' element = {<Login/>}/>
-            <Route path='/register' element = {<Register/>}/>
-        </Routes>            
-    </BrowserRouter>
-    
-  
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 
