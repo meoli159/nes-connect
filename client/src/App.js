b1f43c088e737fab4a5c4cd02a2e0e9d10052434
Nero Tiến <kur0dk0@gmail.com>
2022-06-30 17:43:17 +0700

    register with axios

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import socketIO from 'socket.io-client'
+// import socketIO from 'socket.io-client'
 
 
 
@@ -9,21 +9,23 @@ import socketIO from 'socket.io-client'
 
 // pages
 import Home from "./pages/Home/Home";
-import Login from "./pages/Login/LoginPage";
+import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
 
-const WS ="http://localhost:3333";
+// const WS ="http://localhost:3333";
 
 function App() {
-  useEffect(()=>{
-    socketIO(WS);
-  },[])
+  // useEffect(()=>{
+  //   socketIO(WS);
+  // },[])
+  const[token,setToken] = useState();
+
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/login" element={<Login setToken={setToken}/>} />
           <Route path="/register" element={<Register />} />
         </Routes>
       </BrowserRouter>
--- client/src/pages/Register/Register.jsx
+++ client/src/pages/Register/Register.jsx
@@ -1,7 +1,44 @@
-import React from 'react'
+import React,{ useState} from "react";
+import "./Register.css";
+import request from "../../utils/request";
 
-export default function register() {
+export default function Register() {
+
+  const [username, setUserName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+// const handleChange =(e)=>{
+  
+// };
+
+const handleSubmit= (e)=>{
+  e.preventDefault();
+request.post('/auth/signup',{
+  username,
+  email,
+  password
+})
+.then(res => {
+  console.log(res);
+  console.log(res.data);
+});
+console.log({username,email,password})
+};
+
+
   return (
-    <div>register</div>
-  )
+    <div className="Register">
+      <h1>Register</h1>
+      <form onSubmit={handleSubmit}>
+        <label>User Name</label>
+        <input placeholder="username" type="text" onChange={(e)=> setUserName(e.target.value)} value={username}/>
+        <label>Email</label>
+        <input placeholder="email" type="text" onChange={(e)=> setEmail(e.target.value)} value={email}/>
+        <label>Password</label>
+        <input placeholder="password" type="password" onChange={(e)=> setPassword(e.target.value)} value={password}/>
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
 }
