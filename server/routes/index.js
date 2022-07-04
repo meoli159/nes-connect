d573a0741fbf4208720f7dd8e6d2a7a648dadf7f
Cyrus <74394657+meoli159@users.noreply.github.com>
2022-07-04 12:49:14 +0700

    Dev (#11)
    * fix auth.controller; fix routes signup to register, signin to login
    
    * Update package.json
    
    Co-authored-by: Vinh Vo <voh.vinh803@gmail.com>

--- server/routes/index.js
+++ server/routes/index.js
@@ -17,13 +17,13 @@ router.use(function(req, res, next) {
 });
 
 //register
-router.get("/auth/register",(req,res)=>{
+router.get("/register",(req,res)=>{
     //res.sendFile(path.join(__dirname, '../views', 'register.html'));
     res.render("register");
 })
 
 router.post(
-    "/auth/register",
+    "/register",
     [
         verifySignUp.checkDuplicateUsernameOrEmail,
         //verifySignUp.checkRolesExisted
@@ -32,15 +32,15 @@ router.post(
 );
 
 // đăng nhập
-router.get("/auth/login",(req,res)=>{
+router.get("/login",(req,res)=>{
     //res.sendFile(path.join(__dirname, '../views', 'login.html'));
     res.render("login");
 })
 
-router.post("/auth/login", controller.login);
+router.post("/login", controller.login);
 
 // đăng xuất
-router.get('/auth/logout', function(req, res, next) {
+router.get('/logout', function(req, res, next) {
     // set tuổi cookie về 1mili giây
     res.cookie('jwt', '', { maxAge : 1 });
     res.redirect('/');
