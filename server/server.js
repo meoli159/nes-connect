6d2a9ed7c09be472f62089ecd8f8d3d836862a17
Vinh Vo <voh.vinh803@gmail.com>
2022-10-15 14:46:52 +0700

    update streaming

--- server/server.js
+++ server/server.js
@@ -53,8 +53,12 @@ const io = require("socket.io")(server, {
   },
 });
 
+const users = {};
+
+const socketToRoom = {};
+
 io.on("connection", (socket) => {
-  // console.log("Io connected");
+  //console.log("Io connected");
 
   socket.on("setup", (userData) => {
     socket.join(userData._id);
@@ -78,4 +82,46 @@ io.on("connection", (socket) => {
   socket.off("setup", () => {
     socket.leave(userData._id);
   });
+
+  socket.on("join-stream", stream => {
+    socket.join(stream.streamId);
+    socket.to(stream.streamId).emit('new-user-connect', stream.userId);
+    socket.on('disconnect', () => {
+      socket.to(stream.streamId).emit('user-disconnected', stream.userId);
+    });
+
+    /*
+    if (users[streamID]) {
+      const length = users[roomID].length;
+      if (length === 4) {
+        socket.emit("room full");
+        return;
+      }
+      users[streamID].push(socket.id);
+    } else {
+      users[streamID] = [socket.id];
+    }
+    const usersInThisRoom = users[streamID].filter(id => id !== socket.id);
+
+    socket.emit("all users", usersInThisRoom);*/
+
+  });
+
+  socket.on('newUserStart', (data) => {
+    socket.to(data.to).emit('newUserStart', { sender: data.sender });
+  });
+
+  socket.on("sending signal", payload => {
+    io.to(payload.streamID).emit('user joined', { signal: payload.signal, callerID: payload.callerID });
+  });
+
+  socket.on("returning signal", payload => {
+    io.to(payload.callerID).emit('receiving returned signal', { signal: payload.signal, id: socket.id });
+  });
+
+  socket.on("sendDataClient", function(data) {
+    console.log(data)
+    io.emit("sendDataServer", { data });
+  })
+
 });
