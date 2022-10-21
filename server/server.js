dc522d77f99e87c7366d4ea2280a29f9a9ba534b
Cyrus <kur0dk0@gmail.com>
2022-10-21 15:58:34 +0700

    add node env

--- server/server.js
+++ server/server.js
@@ -26,17 +26,17 @@ app.use("/api/user", user);
 app.use("/api/message", message);
 
 //-------------------Deployment-------------------
-// const __dirname1 = path.resolve();
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname1,'/client/build')));
-//   app.get('*',(req,res)=>{
-//     res.sendFile(path.resolve(__dirname1,"client","build","index.html"))
-//   })
-// } else {
-//   app.get("/", (req, res) => {
-//     res.send("API is running successfully");
-//   });
-// }
+const __dirname1 = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1,'/client/build')));
+  app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(__dirname1,"client","build","index.html"))
+  })
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running successfully");
+  });
+}
 //-------------------Deployment-------------------
 
 //Port
