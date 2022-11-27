fa5c53c8316f25826f8b46b7b66c9ea7fb0d5871
Vinh Vo <voh.vinh803@gmail.com>
2022-11-28 01:30:48 +0700

    test peer

--- client/src/components/StreamContext/StreamContext.jsx
+++ client/src/components/StreamContext/StreamContext.jsx
@@ -15,6 +15,42 @@ const StreamContext = () => {
   var myId = "";
   var peer;
 
+  const iceServers = {
+    'iceServers': [{url:'stun:stun01.sipphone.com'},
+  {url:'stun:stun.ekiga.net'},
+  {url:'stun:stun.fwdnet.net'},
+  {url:'stun:stun.ideasip.com'},
+  {url:'stun:stun.iptel.org'},
+  {url:'stun:stun.rixtelecom.se'},
+  {url:'stun:stun.schlund.de'},
+  {url:'stun:stun.l.google.com:19302'},
+  {url:'stun:stun1.l.google.com:19302'},
+  {url:'stun:stun2.l.google.com:19302'},
+  {url:'stun:stun3.l.google.com:19302'},
+  {url:'stun:stun4.l.google.com:19302'},
+  {url:'stun:stunserver.org'},
+  {url:'stun:stun.softjoys.com'},
+  {url:'stun:stun.voiparound.com'},
+  {url:'stun:stun.voipbuster.com'},
+  {url:'stun:stun.voipstunt.com'},
+  {url:'stun:stun.voxgratia.org'},
+  {url:'stun:stun.xten.com'},
+  {
+    url: 'turn:numb.viagenie.ca',
+    credential: 'muazkh',
+    username: 'webrtc@live.com'
+  },
+  {
+    url: 'turn:192.158.29.39:3478?transport=udp',
+    credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+    username: '28224511:1379330808'
+  },
+  {
+    url: 'turn:192.158.29.39:3478?transport=tcp',
+    credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+    username: '28224511:1379330808'
+  }]};
+
   const [id, setId] = useState();
   const [currentPeer, setPeer] = useState(null);
   const [mystream, setmystream] = useState(null);
@@ -22,8 +58,12 @@ const StreamContext = () => {
   const messagesEnd = useRef();
 
   const socket = useContext(SocketContext);
-  peer = new Peer(user._id);
+  peer = new Peer(user._id,
+    {
+    config: iceServers
+    });
   console.log("peer: " + peer);
+  console.log("peer: " + user._id);
   useEffect(() => {
     const createVideo = (createVideo) => {
       if (!videoContainer[createVideo.id]) {
