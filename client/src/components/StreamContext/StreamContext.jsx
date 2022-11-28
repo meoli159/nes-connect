81116203ca0ef67391bfd935ca363621f1b0b76b
Vinh Vo <voh.vinh803@gmail.com>
2022-11-29 00:24:42 +0700

    set stun server

--- client/src/components/StreamContext/StreamContext.jsx
+++ client/src/components/StreamContext/StreamContext.jsx
@@ -49,20 +49,20 @@ const StreamContext = () => {
         { url: "stun:stun.voxgratia.org" },
         { url: "stun:stun.xten.com" },
         {
-          url: 'turn:numb.viagenie.ca',
-          credential: 'muazkh',
-          username: 'webrtc@live.com'
+          url: "turn:numb.viagenie.ca",
+          credential: "muazkh",
+          username: "webrtc@live.com",
         },
         {
-          url: 'turn:192.158.29.39:3478?transport=udp',
-          credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-          username: '28224511:1379330808'
+          url: "turn:192.158.29.39:3478?transport=udp",
+          credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
+          username: "28224511:1379330808",
         },
         {
-          url: 'turn:192.158.29.39:3478?transport=tcp',
-          credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-          username: '28224511:1379330808'
-        }
+          url: "turn:192.158.29.39:3478?transport=tcp",
+          credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
+          username: "28224511:1379330808",
+        },
       ],
     };
 
