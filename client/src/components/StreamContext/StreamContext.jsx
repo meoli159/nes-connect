df1b882f26a471de7ea5a765ad887f709dacd4cb
Vinh Vo <voh.vinh803@gmail.com>
2022-11-29 02:10:07 +0700

    st new

--- client/src/components/StreamContext/StreamContext.jsx
+++ client/src/components/StreamContext/StreamContext.jsx
@@ -29,25 +29,24 @@ const StreamContext = () => {
 
     const iceServers = {
       iceServers: [
-        { url: "stun:stun01.sipphone.com" },
-        { url: "stun:stun.ekiga.net" },
-        { url: "stun:stun.fwdnet.net" },
-        { url: "stun:stun.ideasip.com" },
-        { url: "stun:stun.iptel.org" },
-        { url: "stun:stun.rixtelecom.se" },
-        { url: "stun:stun.schlund.de" },
-        { url: "stun:stun.l.google.com:19302" },
-        { url: "stun:stun1.l.google.com:19302" },
-        { url: "stun:stun2.l.google.com:19302" },
-        { url: "stun:stun3.l.google.com:19302" },
-        { url: "stun:stun4.l.google.com:19302" },
-        { url: "stun:stunserver.org" },
-        { url: "stun:stun.softjoys.com" },
-        { url: "stun:stun.voiparound.com" },
-        { url: "stun:stun.voipbuster.com" },
-        { url: "stun:stun.voipstunt.com" },
-        { url: "stun:stun.voxgratia.org" },
-        { url: "stun:stun.xten.com" }
+        {
+          urls: "stun:openrelay.metered.ca:80",
+        },
+        {
+          urls: "turn:openrelay.metered.ca:80",
+          username: "openrelayproject",
+          credential: "openrelayproject",
+        },
+        {
+          urls: "turn:openrelay.metered.ca:443",
+          username: "openrelayproject",
+          credential: "openrelayproject",
+        },
+        {
+          urls: "turn:openrelay.metered.ca:443?transport=tcp",
+          username: "openrelayproject",
+          credential: "openrelayproject",
+        }
       ],
     };
 
@@ -202,7 +201,6 @@ const StreamContext = () => {
     const enabled = mystream.getVideoTracks()[0].enabled;
     if (enabled) {
       mystream.getVideoTracks()[0].enabled = false;
-      console.log(mystream.getVideoTracks()[0]);
       setPlayVideo();
     } else {
       setStopVideo();
