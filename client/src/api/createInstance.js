9f955fb7f056fdf263323fd25b07c16f60a8104d
Cyrus <kur0dk0@gmail.com>
2023-11-22 12:48:05 +0700

    Fix env name require REACT_APP,Add env sample

--- client/src/api/createInstance.js
+++ client/src/api/createInstance.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 // import { store } from "../redux/store";
-const API_URL = process.env.BE_URL;
+const API_URL = process.env.REACT_APP_BE_URL;
+
 const axiosClient = axios.create({ baseURL: API_URL });
 
 axiosClient.interceptors.request.use(function (config) {
